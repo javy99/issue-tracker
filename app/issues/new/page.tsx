5e4672d3c424c646a7ceffa281ea1d91f06b4202
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-02 19:22:19 +0200

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
+import dynamic from 'next/dynamic';
 import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import { useForm, Controller } from 'react-hook-form'
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
@@ -11,11 +11,16 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
+
 const NewIssuePage = () => {
     const [error, setError] = useState<string>('')
+    const [isSubmitting, setSubmitting] = useState<boolean>(false)
+
     const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
         resolver: zodResolver(createIssueSchema)
     })
@@ -32,9 +37,11 @@ const NewIssuePage = () => {
                 className='space-y-3'
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setSubmitting(true)
                         await axios.post('/api/issues', data)
                         router.push('/issues')
                     } catch (error) {
+                        setSubmitting(false)
                         setError('An unexpected error occurred.')
                     }
                 })}
@@ -46,7 +53,9 @@ const NewIssuePage = () => {
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>
+                    Submit New Issue {isSubmitting && <Spinner />}
+                </Button>
             </form>
         </div>
 
