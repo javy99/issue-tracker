857405ef5167c1a16c67f8507fcf8d05b1076917
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-03 14:07:08 +0200

    Improve the loading experience

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -43,6 +43,5 @@ const IssuesPage = async () => {
 }
 
 export const dynamic = 'force-dynamic'
-// export const revalidate = 0; // same as export const dynamic = 'force-dynamic'
 
 export default IssuesPage
